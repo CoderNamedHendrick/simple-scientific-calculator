43c4f971daa437c8f601ef82cd8b7ebe523c85f8
Hendrick <sebastinesoacatp@gmail.com>
2021-03-05 19:18:12 +0100

    Adding the helper singleton class

---
+++ app/src/main/java/com/hendrick/simplescientificcalculator/HistoryHelper.java
@@ -0,0 +1,50 @@
+package com.hendrick.simplescientificcalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHelper {
+
+    private static HistoryHelper single_instance = null;
+    private String expression = "";
+
+    public List<String> operations;
+    public String[] operationals =  new String[3];
+    int operationalCount;
+    private HistoryHelper(){
+        operations = new ArrayList<>();
+    }
+
+    public static HistoryHelper getInstance(){
+        if (single_instance == null){
+            single_instance = new HistoryHelper();
+        }
+        return single_instance;
+    }
+
+    public void addToExpression(String operational){
+        operationals[operationalCount] = operational;
+        operationalCount++;
+    }
+
+    private void createExpression(){
+        for (int i = 0; i < operationals.length; i++){
+            expression += operationals[i];
+        }
+    }
+
+    public void addToList(){
+        createExpression();
+        operations.add(expression);
+    }
+
+    public List<String> returnAll(){
+        return operations;
+    }
+
+    public void reset(){
+        operationalCount = 0;
+        expression = "";
+        operationals = new String[3];
+    }
+}
